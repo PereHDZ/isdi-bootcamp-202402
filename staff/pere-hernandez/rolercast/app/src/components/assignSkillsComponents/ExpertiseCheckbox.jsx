b4f5d1f54e33ceb280dc0861c8df20f97fa79bbe
Pere <tisores92@hotmail.com>
2024-05-14 14:53:06 +0200

    Componetize ExpertiseCheckbox in AssignStats #442

---
+++ staff/pere-hernandez/rolercast/app/src/components/assignSkillsComponents/ExpertiseCheckbox.jsx
@@ -0,0 +1,51 @@
+function ExpertiseCheckbox ({ item: [expertisePoints, availableExpertises, checkedExpertises, bonuses, selectedExpertises, allSkills, setCheckedExpertises, setSelectedExpertises] }) {
+    const handleExpertiseCheckboxChange = (event) => {
+        const selectedExpertise = event.target.value
+        let newCheckedExpertises = checkedExpertises
+
+        if (event.target.checked){
+            if (checkedExpertises.length < expertisePoints){
+                newCheckedExpertises = [...selectedExpertises, selectedExpertise]
+
+                setCheckedExpertises(newCheckedExpertises)
+                setSelectedExpertises([...selectedExpertises, selectedExpertise])
+            } else {
+                event.target.checked = false
+                alert(`You can only select up to ${expertisePoints} expertise skills`)
+            }
+        } else {
+            newCheckedExpertises = checkedExpertises.filter(expertise => expertise !== selectedExpertise)
+
+            const newSelectedExpertises = selectedExpertises.filter(expertise => expertise !== selectedExpertise)
+
+            setCheckedExpertises(newCheckedExpertises)
+            setSelectedExpertises([...newSelectedExpertises])
+        }
+    }
+
+    return (
+        <div className='distribute-stats margins margin-top'>
+            <span><strong>SELECT {expertisePoints} SKILLS TO SET AS EXPERTISE</strong></span>
+
+            <div className='skills-div'>
+                {availableExpertises.map(expertise => (
+                    <div className='align-center' key={expertise}>
+                        <input
+                            type='checkbox'
+                            value={expertise}
+                            className='check-box-input'
+                            onChange={handleExpertiseCheckboxChange}
+                            checked={checkedExpertises.includes(expertise)}
+                        />
+                        <label className='skill-check-label'>
+                            {expertise} +{bonuses[allSkills.find(s => s.name === expertise).stat]}
+                        </label>
+                    </div>
+                ))}
+            </div>
+            
+        </div>
+    )
+}
+
+export default ExpertiseCheckbox
--- staff/pere-hernandez/rolercast/app/src/routes/AssignSkills.jsx
+++ staff/pere-hernandez/rolercast/app/src/routes/AssignSkills.jsx
@@ -10,6 +10,7 @@ import ArmourProficiencies from '../components/assignSkillsComponents/ArmourProf
 import SkillProficiencies from '../components/assignSkillsComponents/SkillProficiencies'
 import Expertises from '../components/assignSkillsComponents/Expertises'
 import ConfirmButton from '../components/assignSkillsComponents/ConfirmButton'
+import ExpertiseCheckbox from '../components/assignSkillsComponents/ExpertiseCheckbox'
 
 function AssignSkills ({ onRetrunClick, onSkillsConfirmed }){
     const { race } = useRace()
@@ -384,31 +385,7 @@ function AssignSkills ({ onRetrunClick, onSkillsConfirmed }){
     }
 
 
-    const renderAvailableExpertises = () => {
-        return (
-            <div className='distribute-stats margins margin-top'>
-                <span><strong>SELECT {expertisePoints} SKILLS TO SET AS EXPERTISE</strong></span>
-
-                <div className='skills-div'>
-                    {availableExpertises.map(expertise => (
-                        <div className='align-center' key={expertise}>
-                            <input
-                                type='checkbox'
-                                value={expertise}
-                                className='check-box-input'
-                                onChange={handleExpertiseCheckboxChange}
-                                checked={checkedExpertises.includes(expertise)}
-                            />
-                            <label className='skill-check-label'>
-                                {expertise} +{bonuses[allSkills.find(s => s.name === expertise).stat]}
-                            </label>
-                        </div>
-                    ))}
-                </div>
-                
-            </div>
-        )
-    }
+    
 
     const handleCheckboxChange = (event) => {
         const selectedSkill = event.target.value
@@ -434,30 +411,6 @@ function AssignSkills ({ onRetrunClick, onSkillsConfirmed }){
         }
     }
 
-    const handleExpertiseCheckboxChange = (event) => {
-        const selectedExpertise = event.target.value
-        let newCheckedExpertises = checkedExpertises
-
-        if (event.target.checked){
-            if (checkedExpertises.length < expertisePoints){
-                newCheckedExpertises = [...selectedExpertises, selectedExpertise]
-
-                setCheckedExpertises(newCheckedExpertises)
-                setSelectedExpertises([...selectedExpertises, selectedExpertise])
-            } else {
-                event.target.checked = false
-                alert(`You can only select up to ${expertisePoints} expertise skills`)
-            }
-        } else {
-            newCheckedExpertises = checkedExpertises.filter(expertise => expertise !== selectedExpertise)
-
-            const newSelectedExpertises = selectedExpertises.filter(expertise => expertise !== selectedExpertise)
-
-            setCheckedExpertises(newCheckedExpertises)
-            setSelectedExpertises([...newSelectedExpertises])
-        }
-    }
-
     return <section>
         <ReturnButton onReturnClicked={handleReturnClick}/>
 
@@ -475,7 +428,7 @@ function AssignSkills ({ onRetrunClick, onSkillsConfirmed }){
             </div>
             
             <div className='stats-div'>
-                { availableExpertises.length > 0 && renderAvailableExpertises() }   
+                <ExpertiseCheckbox item={[expertisePoints, availableExpertises, checkedExpertises, bonuses, selectedExpertises, allSkills, setCheckedExpertises, setSelectedExpertises]}/>
             </div>            
         </div>
 
